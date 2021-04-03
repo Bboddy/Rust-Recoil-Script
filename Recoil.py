065b21273badcff65b8b530b6a41e4b7afec5a38
Bboddy <43559704+Bboddy@users.noreply.github.com>
2021-04-02 18:11:20 -0700

    Added Loader from (Login-PythonGui)
    Renamed Recoil-Script.py to Recoil.py

--- Recoil.py
+++ Recoil.py
@@ -1,17 +1,13 @@
-import win32api, ctypes, time, pyttsx3, random
+import win32api
+import ctypes
+import time
+import pyttsx3
+import random
+from numpy import random
 
 #Loop settings
 active = True
 paused = False
-drawDot = True
-#TTS Settings
-engine = pyttsx3.init()
-engine.setProperty("volume", 0.5)
-engine.setProperty("rate", 350)
-voices = engine.getProperty("voices")
-engine.setProperty("voice", voices[1].id)
-engine.say("Started")
-engine.runAndWait() #Run engine.say and wait till done
 ########### Recoil Tables
 Recoil_AK = [[-36.3583, 52.3906], [5.6019, 48.1107], [-57.7970, 43.7258], [-44.4413, 39.2358], [-0.1867, 34.6408], [17.4687, 29.9408], [30.7335, 25.1358], [39.6080, 20.2258], [44.0919, 15.2107], [44.1854, 10.0906], [39.8884, 10.3678], [31.2010, 19.1802], [18.1232, 26.1569], [0.9701, 31.1046], [-15.7055, 34.0233], [-28.9507, 34.9129], [-38.7704, 33.7734], [-45.1648, 30.6050], [-48.1337, 25.4073], [-47.6774, 18.1807], [-43.7956, 8.9251], [-36.4885, 5.1987], [-25.7559, 14.5390], [-11.5980, 21.8952], [12.5535, 26.8223], [37.8676, 29.3207], [50.8869, 29.3899], [51.5930, 27.0302], [39.9856, 22.2415]]
 Recoil_LR = [[-2.5716, 26.2726], [-6.499, 32.5123], [-10.5691, 34.6882], [-15.0501, 32.8004], [-16.5015, 26.8927], [-14.8903, 21.6664], [-10.2167, 18.6228], [-2.3359, 15.8424], [9.5645, 13.3251], [18.0725, 11.0709], [21.0806, 9.0797], [18.5887, 7.3517], [10.5968, 5.9258], [-0.4584, 5.1813], [-5.8302, 4.6544], [-9.7352, 4.1882], [-12.7238, 3.7826], [-14.7961, 3.4377], [-15.952, 3.1534], [-16.1917, 2.9299], [-15.5149, 2.7669], [-13.9219, 2.6647], [-11.4124, 2.6231], [-7.9867, 2.6421], [-3.5444, 2.7219], [14.0846, 2.8623], [32.0283, 3.0633], [37.866, 3.325], [31.5974, 3.6474], [0, 8], [50, 0]]
@@ -80,7 +76,7 @@ def mouse_move_random(x,y,draw_delay):
     # print(x, y, "=>", round(x), round(y))
 
 def mouse_move(x,y,draw_delay):
-    divider = random.randint(20,35)
+    divider = random.randint(25,100)
     start_time = time.perf_counter()
     moveindex = 0
     dxindex = 0
@@ -90,52 +86,23 @@ def mouse_move(x,y,draw_delay):
     dy = int(y / divider)
     ry = y % divider
     while moveindex < divider:
-        ctypes.windll.user32.mouse_event(0x0001, dx, dy, 0, 0) #Move recoil / divider
+        ctypes.windll.user32.mouse_event(0x0001, dx, dy, 0, 5) #Move recoil / divider
         moveindex += 1
         if absx * moveindex  > (dxindex + 1) * divider:
             dxindex += 1
-            ctypes.windll.user32.mouse_event(0x0001, int(x/abs(x)), 0, 0, 0)
+            ctypes.windll.user32.mouse_event(0x0001, int(x/abs(x)), 0, 0, 5)
         if ry * moveindex  > (dyindex + 1) * divider:
             dyindex += 1
-            ctypes.windll.user32.mouse_event(0x0001, 0, int(y/abs(y)), 0, 0)
+            ctypes.windll.user32.mouse_event(0x0001, 0, int(y/abs(y)), 0, 5)
     if x != 0 and y != 0:
         if round(x) != dxindex*int(x/abs(x))+dx*moveindex:
-            ctypes.windll.user32.mouse_event(0x0001, int(x/abs(x)), 0, 0, 0)
+            ctypes.windll.user32.mouse_event(0x0001, int(x/abs(x)), 0, 0, 5)
             dxindex += 1
         if round(y) != dyindex*int(y/abs(y))+dy*moveindex:
-            ctypes.windll.user32.mouse_event(0x0001, int(y/abs(y)), 0, 0, 0)
+            ctypes.windll.user32.mouse_event(0x0001, int(y/abs(y)), 0, 0, 5)
             dyindex += 1
     time.sleep(draw_delay - (time.perf_counter() - start_time))
 #print(x, y, "=>", dxindex*int(x/abs(x))+dx*moveindex, dyindex*int(y/abs(y))+dy*moveindex)
-
-def mouse_move_rng(x,y,draw_delay):
-    divider = random.randint(20,35)
-    start_time = time.perf_counter()
-    moveindex = 0
-    dxindex = 0
-    dyindex = 0
-    dx = int(x / divider)
-    absx = abs(x - dx * divider)
-    dy = int(y / divider)
-    ry = y % divider
-    while moveindex < divider:
-        rng = random.randrange(-0.7, 0.7)
-        ctypes.windll.user32.mouse_event(0x0001, dx, dy, 0, 0) #Move recoil / divider
-        moveindex += 1
-        if absx * moveindex  > (dxindex + 1) * divider:
-            dxindex += 1
-            ctypes.windll.user32.mouse_event(0x0001, int(x/abs(x) + rng), 0, 0, 0)
-        if ry * moveindex  > (dyindex + 1) * divider:
-            dyindex += 1
-            ctypes.windll.user32.mouse_event(0x0001, 0, int(y/abs(y) + rng), 0, 0)
-    if x != 0 and y != 0:
-        if round(x) != dxindex*int(x/abs(x))+dx*moveindex:
-            ctypes.windll.user32.mouse_event(0x0001, int(x/abs(x)), 0, 0, 0)
-            dxindex += 1
-        if round(y) != dyindex*int(y/abs(y))+dy*moveindex:
-            ctypes.windll.user32.mouse_event(0x0001, int(y/abs(y)), 0, 0, 0)
-            dyindex += 1
-    time.sleep(draw_delay - (time.perf_counter() - start_time))
 
 def call_move(draw_pattern, delay):
     current_index = 0
@@ -187,36 +154,49 @@ def call_recoil_control(): #Passing control() the correct values
 #Setting sense
 get_sense()
 
-while active: #Main loop
-    #While not paused
-    if not paused:
-        if win32api.GetKeyState(0x01) < 0: # and win32api.GetKeyState(0x02) < 0:
-            call_recoil_control()
-        if win32api.GetKeyState(0x22) < 0: #PageDown
-            active_weapon = weapon_change(1)
-            engine.say(all_weapons[active_weapon])
+def run():
+    global active, paused, active_weapon, active_scope_value
+    #TTS Settings
+    engine = pyttsx3.init()
+    engine.setProperty("volume", 0.5)
+    engine.setProperty("rate", 350)
+    voices = engine.getProperty("voices")
+    engine.setProperty("voice", voices[1].id)
+    engine.say("Started")
+    engine.runAndWait() #Run engine.say and wait till done
+    while active: #Main loop
+        #While not paused
+        if not paused:
+            if win32api.GetKeyState(0x01) < 0 and win32api.GetKeyState(0x02) < 0:
+                call_recoil_control()
+            if win32api.GetKeyState(0x22) < 0: #PageDown
+                #win32api.SetCursorPos([300, 300]) #For drawing in paint (debugging)
+                active_weapon = weapon_change(1)
+                engine.say(all_weapons[active_weapon])
+                engine.runAndWait()
+            if win32api.GetKeyState(0x21) < 0: #PageUp
+                active_weapon = weapon_change(-1)
+                engine.say(all_weapons[active_weapon])
+                engine.runAndWait()
+            if win32api.GetKeyState(0x24) < 0: #Home
+                active_scope = scope_change()
+                active_scope_value = get_active_scope_value()
+                engine.say(all_scopes[active_scope])
+                engine.runAndWait()
+        #Doesnt Matter if Paused
+        if win32api.GetKeyState(0x91) < 0: #ScrLk
+            get_sense()
+            engine.say("Updated")
             engine.runAndWait()
-        if win32api.GetKeyState(0x21) < 0: #PageUp
-            active_weapon = weapon_change(-1)
-            engine.say(all_weapons[active_weapon])
-            engine.runAndWait()
-        if win32api.GetKeyState(0x24) < 0: #Home
-            active_scope = scope_change()
-            active_scope_value = get_active_scope_value()
-            engine.say(all_scopes[active_scope])
-            engine.runAndWait()
-    #Doesnt Matter if Paused
-    if win32api.GetKeyState(0x91) < 0: #ScrLk
-        get_sense()
-    if win32api.GetKeyState(0x13) < 0: #Pause
-        paused = not paused
-        if paused:
-            engine.say("Paused")
-            engine.runAndWait()
-        elif not paused:
-            engine.say("Unpaused")
-            engine.runAndWait()
-    if win32api.GetKeyState(0x23) < 0: #End 
-            engine.say("Exiting")
-            engine.runAndWait()
-            active = False
+        if win32api.GetKeyState(0x13) < 0: #Pause
+            paused = not paused
+            if paused:
+                engine.say("Paused")
+                engine.runAndWait()
+            elif not paused:
+                engine.say("Unpaused")
+                engine.runAndWait()
+        if win32api.GetKeyState(0x23) < 0: #End 
+                engine.say("Exiting")
+                engine.runAndWait()
+                active = False
